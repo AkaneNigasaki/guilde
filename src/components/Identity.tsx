4d8ee6cac21b957f1ccb8dbbb22263b3dc5e65e7
Akane Nigasaki <192503732+AkaneNigasaki@users.noreply.github.com>
2026-09-09 19:30:40 +0300

    Test

--- src/components/Identity.tsx
+++ src/components/Identity.tsx
@@ -43,6 +43,7 @@ export default function Identity() {
 
     return (
         <section className="relative overflow-hidden bg-[#0B0B0E] py-32 lg:py-48">
+            test
             <div className="mx-auto max-w-[1600px] px-6 lg:px-12">
                 {/* Section header */}
                 <div
